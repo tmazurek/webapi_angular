dd438cf7e19c60280d51689db08c379476529ca4
Tomasz Mazurek <tmazurek@windowslive.com>
2014-01-13 12:59:07 +0100

    more szit

---
+++ WebApplication1/WebApplication1/AppStart/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.AppStart
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/FlightsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FlightsController : ApiController
+    {
+        //
+        // GET: /Flights/
+        public int Get()
+        {
+            return 42;
+        }
+    }
+}
