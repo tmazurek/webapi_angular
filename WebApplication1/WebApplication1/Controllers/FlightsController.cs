91bd449d0da29142371d5cf7766dd9bb1b6e8efc
Tomasz Mazurek <tmazurek@windowslive.com>
2014-01-13 20:43:37 +0100

    after update and controller db context

--- WebApplication1/WebApplication1/Controllers/FlightsController.cs
+++ WebApplication1/WebApplication1/Controllers/FlightsController.cs
@@ -14,13 +14,14 @@ namespace WebApplication1.Controllers
         // GET: /Flights/
         public IHttpActionResult GetAllFlights()
         {
-            var model = new List<Flight>
-            {
-                new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=(DateTime.Now.AddHours(3)), FlightNumber="122", Price=654.30, StartTime= DateTime.Now, TotalDuration=2},
-                new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=DateTime.Now.AddHours(3), FlightNumber="122", Price=654.30, StartTime= DateTime.Now, TotalDuration=2}
             
-            };
-            return Ok(model) ;
+                //new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=(DateTime.Now.AddHours(3)), FlightNumber="122", Price=654.30, StartTime= DateTime.Now, TotalDuration=2},
+                //new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=DateTime.Now.AddHours(3), FlightNumber="122", Price=654.30, StartTime= DateTime.Now, TotalDuration=2}
+            var db = new FlightDb();
+
+
+           
+            return Ok(db.Flights) ;
             
         }
     }
