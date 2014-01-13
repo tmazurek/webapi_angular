882817335f5a40135be8ee507ca028b9029199bb
Tomasz Mazurek <tmazurek@windowslive.com>
2014-01-13 15:31:02 +0100

    dasd

--- WebApplication1/WebApplication1/Controllers/FlightsController.cs
+++ WebApplication1/WebApplication1/Controllers/FlightsController.cs
@@ -16,8 +16,8 @@ namespace WebApplication1.Controllers
         {
             var model = new List<Flight>
             {
-                new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=(DateTime.Now.AddHours(3)), FlightNumber="122", Prize=654.30, StartTime= DateTime.Now, TotlaDuration=2},
-                new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=DateTime.Now.AddHours(3), FlightNumber="122", Prize=654.30, StartTime= DateTime.Now, TotlaDuration=2}
+                new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=(DateTime.Now.AddHours(3)), FlightNumber="122", Price=654.30, StartTime= DateTime.Now, TotalDuration=2},
+                new Flight(){ID=1, Airline="from mdel", Approved=false, Cities= new[]{"ny","org"}, Date=new DateTime(2013,01,12), EndTime=DateTime.Now.AddHours(3), FlightNumber="122", Price=654.30, StartTime= DateTime.Now, TotalDuration=2}
             
             };
             return Ok(model) ;
--- WebApplication1/WebApplication1/Model/Flight.cs
+++ WebApplication1/WebApplication1/Model/Flight.cs
@@ -15,8 +15,8 @@ namespace WebApplication1.Model
         
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public int TotlaDuration { get; set; }
-        public double Prize { get; set; }
+        public int TotalDuration { get; set; }
+        public double Price { get; set; }
         public DateTime Date { get; set; }
         public string[] Cities { get; set; }
         public bool Approved { get; set; }
