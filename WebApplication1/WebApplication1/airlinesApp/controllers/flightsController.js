37fa671100ed0afb9e47c81c903ef8f0be6a188b
Tomasz Mazurek <tmazurek@windowslive.com>
2014-01-13 08:59:40 +0100

    asd

--- WebApplication1/WebApplication1/airlinesApp/controllers/flightsController.js
+++ WebApplication1/WebApplication1/airlinesApp/controllers/flightsController.js
@@ -1,46 +1,34 @@
-﻿(function (app) {
+﻿(function (app) 
+{
 
-    var flightsController = function ($scope) {
+    var flightsController = function ($scope, flightService, $timeout) {
 
-        
+        var init = function () {
+            $scope.flights = flightService.getFlights();
+        };
+
+
         $scope.selectedFlight = function (flight) {
-            for (var f in $scope.flights) {
-                $scope.flights[f].approved = false;
+            for (var i = 0; i < $scope.flights.length; i++) {
+                $scope.flights[i].approved = false;
             }
-            flight.approved = true;
+        
+            flight.approved = !flight.approved;
+            if (flightService.selectFlight(flight.id))
+            {
+                $scope.status = "yes u did";
+                $timeout(function () {
+                    $scope.status = null;
+                }, 3000)
+            };
+
         };
+        
+        init();
 
-                                            
-        $scope.flights = 
+    };
+    app.controller("flightsController", ["$scope", "flightService", "$timeout", flightsController]);
+    
 
-            [
-                {
-                airline: "united",
-                flightNumber: 207,
-                startTime: "12:00",
-                endTime: "13:00",
-                totalDuration: 2,
-                price: 400,
-                date: "12/1/2012",
-                cities: ["ORG", "WAS"],
-                approved:false
-
-                },
-                {
-                airline: "lot",
-                flightNumber: 233,
-                startTime: "15:00",
-                endTime: "17:00",
-                totalDuration: 2,
-                price: 500,
-                date: "12/1/2012",
-                cities: ["ORG", "WAS"],
-                approved:false
-                },
-            ]
-        };
-
-
-app.controller("flightsController", ["$scope",flightsController]);
-
+    
 }(angular.module("airlineApp")));
---
+++ WebApplication1/WebApplication1/airlinesApp/services/flightService.js
@@ -0,0 +1,53 @@
+﻿(function (app) {
+
+    var testData = [
+                {
+                    id:1,
+                    airline: "united",
+                    flightNumber: 207,
+                    startTime: "12:00",
+                    endTime: "13:00",
+                    totalDuration: 2,
+                    price: 400,
+                    date: "12/1/2012",
+                    cities: ["ORG", "WAS"],
+                    approved: false
+
+                },
+                {
+                    id:2,
+                    airline: "lot",
+                    flightNumber: 233,
+                    startTime: "15:00",
+                    endTime: "17:00",
+                    totalDuration: 2,
+                    price: 500,
+                    date: "12/1/2012",
+                    cities: ["ORG", "WAS"],
+                    approved: false
+                },
+    ];
+
+    var flightService = function ()
+    {
+        var flightFactory = {};
+
+        flightFactory.getFlights = function ()
+        {
+        return testData;         
+        };
+
+        flightFactory.selectFlight = function ()
+        {
+            //
+            // $http.put("/flights/select/3", id);
+            //
+            return true;
+        };
+
+        return flightFactory;
+    };
+
+    app.factory("flightService", flightService);
+
+}(angular.module("airlineApp")));
